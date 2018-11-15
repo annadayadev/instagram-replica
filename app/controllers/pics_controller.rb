4e771366d649510706aa6e579e7be6bf78cc314f
annadayadev <ryry11adaya@gmail.com>
2018-11-15 15:04:49 +0800

    Edit, Update, and Destroy Actions and View Files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,12 +2,11 @@ class PicsController < ApplicationController
 	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@pics = Pic.all.order("created_at DESC")
 	end
 
 	def show
 	end
-
-
 
 	def new
 		#we'll be using this in our viewfile 
@@ -24,6 +23,23 @@ class PicsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+#update is to edit but create is to new-- responsible for the view files. create and update do not have view files. They are only responsible for changes that are made in the database. 
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Congrats! Pic was updated!"
+		else
+			render 'edit'
+	end
+end
+
+def destroy
+	@pic.destroy
+	redirect_to root_path
+end 
+
 #the reason why we are putting some on private so that it applies to all the other actions instead of repeating ourselves.
 
 	private
@@ -35,5 +51,4 @@ class PicsController < ApplicationController
 	def find_pic
 		@pic = Pic.find(params[:id])
 	end
-
 end
