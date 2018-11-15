c99db3a10331fdf2e3cc4ff9761ddfd6b103e88c
annadayadev <ryry11adaya@gmail.com>
2018-11-15 14:04:26 +0800

    Create new pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,5 +4,21 @@ class PicsController < ApplicationController
 	end
 
 
-	
+	def new
+		#we'll be using this in our viewfile 
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+#the reason why we are putting some on private so that it applies to all the other actions instead of repeating ourselves.
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
+	end
+
 end
